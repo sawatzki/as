d52d494e43f0485a9dd286e94645d5a999fe8ecb
Artem Sawatzki <artsawatzki@gmail.com>
2019-04-26 12:24:33 +0200

    AS-8: users-show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,11 +2,25 @@
 
 @section('content')
 
-    @foreach($users as $user)
-        <a href="{{ route('user_details', ['user'=>$user]) }}">
-        {{ $user->id . ": " . $user->name }} <br>
-        {{ $user->email }} <hr>
-        </a>
-    @endforeach
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <a href="{{ route('user_details', ['user'=>$user]) }}">
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+            </a>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
