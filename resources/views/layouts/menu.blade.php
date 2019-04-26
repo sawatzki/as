1ae49b0d575d01d0d1893f90ee1925752429a0f9
Artem Sawatzki <artsawatzki@gmail.com>
2019-04-26 09:13:34 +0200

    AS-6: menu have routes now

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,10 +2,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Users</a>
+                <a class="nav-link" href="{{ route('show_users') }}">Users</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Roles</a>
+                <a class="nav-link" href="{{ route('show_roles') }}">Roles</a>
             </li>
         </ul>
     </div>
